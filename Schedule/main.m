15abd7e9cda04fcb0bf75d712d6e3692a37cba32
Sjostrand Gereholt Amelie <sjostrandgereholtamelie@Sjostrand-Gereholt-Amelies-MacBook-Pro.local>
2013-04-08 18:13:57 +0200

    Initial Commit

---
+++ Schedule/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Schedule
+//
+//  Created by Sjostrand Gereholt Amelie on 2013-04-08.
+//  Copyright (c) 2013 Sjostrand Gereholt Amelie. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
