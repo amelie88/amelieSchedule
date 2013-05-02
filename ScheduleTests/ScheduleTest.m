69c51da31eb447c1895d918c2ad39c35192e8219
Sjostrand Gereholt Amelie <sjostrandgereholtamelie@Sjostrand-Gereholt-Amelies-MacBook-Pro.local>
2013-05-02 16:26:00 +0200

    Klar version

--- ScheduleTests/ScheduleTest.m
+++ ScheduleTests/ScheduleTest.m
@@ -6,177 +6,159 @@
 //  Copyright (c) 2013 Sjostrand Gereholt Amelie. All rights reserved.
 //
 
-//#import "ScheduleTest.h"
-////#import "StudentService.h"
-////#import "CourseService.h"
-//#import "Student.h"
-//#import "Course.h"
-//#import "Admin.h"
-////#import "Course+Json.h"
-////#import "Student+Json.h"
-//
-//static NSString * const englishKey = @"english_key";
-//static NSString * const historyKey = @"history_key";
-//static NSString * const allSubjectsKey = @"allsubjects_key";
-//static NSString *const mondayKey = @"monday_key";
-//static NSString *const tuesdayKey = @"tuesday_key";
-//static NSString *const wednesdayKey = @"wednesday_key";
-//static NSString *const thursdayKey = @"thursday_key";
-//static NSString *const fridayKey = @"friday_key";
-//static NSString *const allWeekdaysKey = @"allweekdays_key";
-//
-//@implementation ScheduleTest
-//
-//{
-//    StudentService *studentservice;
-//    NSDictionary *students;
-//    CourseService *courseservice;
-//    NSDictionary *courses;
-//}
-//
-//- (void)setUp
-//{
-//    studentservice = [[StudentService alloc] init];
-//    students = @{englishKey: [[NSMutableSet alloc] init],
-//                 historyKey:[[NSMutableSet alloc] init],
-//                 allSubjectsKey: [[NSMutableSet alloc] init]};
-//    
-//    courseservice = [[CourseService alloc] init];
-//    courses = @{mondayKey: [[NSMutableSet alloc] init],
-//                tuesdayKey: [[NSMutableSet alloc] init],
-//                wednesdayKey: [[NSMutableSet alloc] init],
-//                thursdayKey: [[NSMutableSet alloc] init],
-//                fridayKey: [[NSMutableSet alloc] init],
-//                allWeekdaysKey: [[NSMutableSet alloc] init]};
-//    
-//}
-//
-//-(void)tearDown
-//{
-//    studentservice = nil;
-//    students = nil;
-//    courseservice = nil;
-//    courses = nil;
-//}
-//
-//
-//
-//- (void)testAddStudent
-//{
-//    Student *Amelie = [[Student alloc] initWithName:@"Amelie" allCourses:@"yes" history:@"yes" english:@"yes"];
-//    
-//    BOOL result = [studentservice addStudent:Amelie];
-//    
-//    STAssertTrue(result, @"Adding a student should be possible");
-//}
-//
-//-(void)testAddCourse
-//{
-//    Course *historyMonday = [[Course alloc] initWithCourseName:@"history" weekday:@"monday" time:@"10-12" teacher:@"Bert Karlsson" classroom:@"1A" chapter:@"3-5" message:@"Ojoj" _id:[[NSUUID UUID] UUIDString]];
-//    Admin *admin = [[Admin alloc] initWithUsername:@"admin" password:@"mySecretPassword"];
-//    
-//    BOOL result = [courseservice addCourse:historyMonday:admin];
-//    
-//    STAssertTrue(result, @"Adding a student should be possible");
-//}
-//
-//-(void)testRemoveStudent
-//{
-//    Student *Amelie = [[Student alloc] initWithName:@"Amelie" allCourses:@"no" history:@"yes" english:@"no"];
-//    
-//    BOOL addResult = [studentservice addStudent:Amelie];
-//    STAssertTrue(addResult, @"Student was added");
-//    
-//    BOOL deletedResult = [studentservice removeStudent:Amelie];
-//    STAssertTrue(deletedResult, @"Student was deleted");
-//}
-//
-//-(void)testSaveStudent
-//{
-//    Student *Amelie = [[Student alloc] initWithName:@"Amelie" allCourses:@"no" history:@"yes" english:@"no"];
-//    
-//    BOOL saveStudentResult = [studentservice saveStudent:Amelie];
-//    STAssertTrue(saveStudentResult, @"Student was saved");
-//    
-//}
-//
-//-(void)testSaveCourse
-//{
-//    Course *historyMonday = [[Course alloc] initWithCourseName:@"history" weekday:@"monday" time:@"10-12" teacher:@"Bert Karlsson" classroom:@"1A" chapter:@"3-5" message:@"Ojoj" _id:[[NSUUID UUID] UUIDString]];
-//    
-//    BOOL saveCourseResult = [courseservice saveCourse:historyMonday];
-//    STAssertTrue(saveCourseResult, @"Course was saved");
-//    
-//}
-//
-//-(void)testUpdateCourse
-//{
-//    Course *historyMonday = [[Course alloc] initWithCourseName:@"history" weekday:@"monday" time:@"10-12" teacher:@"Bert Karlsson" classroom:@"1A" chapter:@"3-5" message:@"Ojoj" _id:[[NSUUID UUID] UUIDString]];
-//    
-//    BOOL updateCourseResult = [courseservice updateCourse:historyMonday :@"1218190B-F843-4FDF-96A9-2A7309ECDE67" :@"1-93369076be98eaa0fbc1576972f80c0a"];
-//    STAssertTrue(updateCourseResult, @"Course was updated");
-//    
-//}
-//
-//-(void)testDeleteCourse
-//{
-//    Course *historyMonday = [[Course alloc] initWithCourseName:@"history" weekday:@"monday" time:@"10-12" teacher:@"Bert Karlsson" classroom:@"1A" chapter:@"3-5" message:@"Ojoj" _id:[[NSUUID UUID] UUIDString]];
-//    Admin *admin = [[Admin alloc] initWithUsername:@"admin" password:@"mySecretPassword"];
-//    
-//    BOOL addResult = [courseservice addCourse:historyMonday :admin];
-//    STAssertTrue(addResult, @"Student was added");
-//    
-//    
-//    BOOL deleteCourseResult = [courseservice deleteCourse:historyMonday :@"1218190B-F843-4FDF-96A9-2A7309ECDE67" :@"1-93369076be98eaa0fbc1576972f80c0a"];
-//    STAssertTrue(deleteCourseResult, @"Course was deleted");
-//}
-//
-////-(void)testGetFromDatabase
-////{
-////    BOOL result = [courseservice getFromDatabase:@"1218190B-F843-4FDF-96A9-2A7309ECDE67" onCompletion:^(NSArray *allReadCourses){
-////                  for(id _id in allReadCourses){
-////                      NSLog(@"%@", [[NSString alloc] initWithData:_id encoding:NSUTF8StringEncoding]);
-////                 }}];
-////} [[[NSRunLoop currentRunLoop] run]];
-////
-////    STAssertTrue(result, @"Course was updated");
-////}
-//
-//
-//-(void)testWeekSchedule
-//{
-//    Student *Amelie = [[Student alloc] initWithName:@"Amelie" allCourses:@"no" history:@"yes" english:@"no"];
-//    
-//    BOOL result = [courseservice weekSchedule:Amelie];
-//    STAssertTrue(result, @"Student can see schedule for week");
-//}
-//
-//
-//-(void)testScheduleForDay
-//{
-//    Student *Amelie = [[Student alloc] initWithName:@"Amelie" allCourses:@"no" history:@"yes" english:@"no"];
-//    
-//    BOOL result = [courseservice scheduleForDay:@"monday":Amelie];
-//    STAssertTrue(result, @"Student can see schedule for day");
-//}
-//
-//
-//-(void)testChapterForDay
-//{
-//    Student *Amelie = [[Student alloc] initWithName:@"Amelie" allCourses:@"no" history:@"yes" english:@"no"];
-//    
-//    BOOL result = [courseservice chapterForDay:@"monday":Amelie];
-//    STAssertTrue(result, @"Student can see chapter for day");
-//}
-//
-//
-//-(void)testChaptersForWeek
-//{
-//    Student *Amelie = [[Student alloc] initWithName:@"Amelie" allCourses:@"no" history:@"yes" english:@"no"];
-//    
-//    BOOL result = [courseservice chaptersForWeek:Amelie];
-//    STAssertTrue(result, @"Student can see chapters for week");
-//}
-//
-//
-//@end
+#import "ScheduleTest.h"
+#import "StudentAndCourseService.h"
+#import "Student.h"
+#import "Course.h"
+#import "Message.h"
+#import "Course+Json.h"
+#import "Student+Json.h"
+#import "Message+Json.h"
+
+
+static NSString * const allSubjectsKey = @"allsubjects_key";
+static NSString *const allWeekdaysKey = @"allweekdays_key";
+static NSString *const allMessagesKey = @"allmessages_key";
+
+@implementation ScheduleTest
+
+{
+    StudentAndCourseService *service;
+    NSDictionary *students;
+    NSDictionary *courses;
+    NSDictionary *messages;
+    Student *student;
+    Course *course;
+    Message *message;
+}
+
+- (void)setUp
+{
+    service = [[StudentAndCourseService alloc] init];
+    students = @{allSubjectsKey: [[NSMutableSet alloc] init]};
+    courses = @{allWeekdaysKey: [[NSMutableSet alloc] init]};
+    messages = @{allMessagesKey: [[NSMutableSet alloc] init]};
+    student = [[Student alloc] initWithName:@"Amelie" type:@"Student" allCourses:@"Yes" history:@"Yes" english:@"Yes" _id:@"Amelie"];
+    course = [[Course alloc] initWithCourseName:@"History" type:@"Course" weekday:@"Monday" time:@"10-12" teacher:@"Karin Bengtson" classroom:@"1A" chapter:@"1" _id:@"historyMonday"];
+    message = [[Message alloc] initWithReceiver:@"Amelie" type:@"Message" studentsMessage:@"Hej alla" privateMessage:@"Hej elev" _id:@"myMessage"];
+}
+
+-(void)tearDown
+{
+    service = nil;
+    courses = nil;
+    students = nil;
+    messages = nil;
+}
+
+
+
+- (void)testAddStudent
+{
+    BOOL result = [service addStudent:student];
+    
+    STAssertTrue(result, @"Adding a student should be possible");
+}
+
+-(void)testAddCourse
+{
+    BOOL result = [service addCourse:course];
+    
+    STAssertTrue(result, @"Adding a student should be possible");
+}
+
+-(void)testAddMessage
+{
+    BOOL result = [service addMessage:message];
+    
+    STAssertTrue(result, @"Adding a message should be possible");
+}
+
+-(void)testSaveCourse
+{
+    BOOL result = [service saveCourse:course:@"mypassword"];
+    STAssertTrue(result, @"Course should be saved");
+}
+
+-(void)testSaveStudent
+{
+    BOOL result = [service saveStudent:student];
+    STAssertTrue(result, @"Student should be saved");
+}
+
+
+-(void)testSaveMessage
+{
+    BOOL result = [service saveMessage:message:@"mypassword"];
+    STAssertTrue(result, @"Message should be saved");
+    
+}
+
+-(void)testUpdateCourse
+{
+    BOOL result = [service updateCourse:course :@"1218190B-F843-4FDF-96A9-2A7309ECDE67" :@"1-93369076be98eaa0fbc1576972f80c0a" :@"mypassword"];
+    STAssertTrue(result, @"Course should be updated");
+}
+
+-(void)testUpdateMessage
+{
+    BOOL result = [service updateMessage:message :@"1218190B-F843-4FDF-96A9-2A7309ECDE67" :@"1-93369076be98eaa0fbc1576972f80c0a" :@"mypassword"];
+    STAssertTrue(result, @"Message should be updated");
+    
+}
+
+-(void)testLoadWithId
+{
+    BOOL result = [service loadWithId:@"Amelie"];
+    STAssertTrue(result, @"Data should be fetched from DB");
+}
+
+-(void)testLoadWithView
+{
+    BOOL result = [service loadWithView:@"publicmessage]"];
+    STAssertTrue(result, @"Data should be fetched from DB");
+}
+
+
+-(void)testScheduleForWeek
+{
+    BOOL result = [service scheduleForWeek:student];
+    STAssertTrue(result, @"Student should see week schedule");
+}
+
+
+-(void)testScheduleForDay
+{
+    BOOL result = [service scheduleForDay:@"monday":student];
+    STAssertTrue(result, @"Student should see schedule for day");
+}
+
+
+-(void)testChapterForDay
+{
+    BOOL result = [service chapterForDay:@"monday":student];
+    STAssertTrue(result, @"Student should chapter for day");
+}
+
+
+-(void)testChaptersForWeek
+{
+    BOOL result = [service chaptersForWeek:student];
+    STAssertTrue(result, @"Student should see chapters for week");
+}
+
+-(void)testUpdateMessages
+{
+    BOOL result = [service updateMessages:message :@"myMessage" :@"4-05eae4e791fab8665148b488e61d9e69" :@"Hej" :@"Huj" :@"Amelie" :@"mypassword"];
+        STAssertTrue(result, @"Messages should be updated");
+}
+
+-(void)testShowMessages
+{
+    BOOL result = [service showMessages:student :message];
+    STAssertTrue(result, @"Student should see messages");
+}
+
+
+
+@end
